ece3fb8542767ed9e043d99aaecc20a334e23bd0
SeanSin <seansin@chungbuk.ac.kr>
2020-10-07 17:51:39 +0900

    add : 기초 도메인 객체 추가

---
+++ src/main/java/BlackJack.java
@@ -0,0 +1,8 @@
+import ui.Game;
+
+public class BlackJack {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+}
---
+++ src/main/java/ui/Game.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class Game {
+    public void play() {
+        System.out.println("===== BLACK JACK =====");
+    }
+}
