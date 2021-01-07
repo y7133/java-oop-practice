bd5ad3a3dc0ddcf9a07d660cfd5e4154bde2bf93
y7133@naver.com <y7133@naver.com>
2021-01-07 19:02:43 +0900

    new branch & final code

--- src/test/java/domain/CardDeckTest.java
+++ src/test/java/domain/CardDeckTest.java
@@ -1,15 +1,25 @@
 package domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CardDeckTest {
 
-
     //카드 섞는 것
     @Test
     void CardShuffle(){
-      //  CardDeck cardDeck = new CardDeck();
-       // System.out.println(cardDeck.cardList.get(0).getCardShape().getShape());
-        //System.out.println(cardDeck.cardList.get(0).getPattern().getName());
+        CardDeck cardDeck = new CardDeck();
+        System.out.println(cardDeck.cardList.get(0).getCardShape().getShape());
+        System.out.println(cardDeck.cardList.get(0).getPattern().getName());
     }
+
+    //카드 뽑고 제거
+    @Test
+    void cardPick(){
+        CardDeck cardDeck = new CardDeck();
+        Card card = cardDeck.cardList.get(0);
+        Assertions.assertEquals(cardDeck.pick(),card);
+        Assertions.assertEquals(cardDeck.cardList.contains(card),false);
+    }
+
 }
--- src/test/java/domain/CardsTest.java
+++ src/test/java/domain/CardsTest.java
@@ -1,18 +1,26 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CardsTest {
 
-//    @Test
-//    @DisplayName("Make Cards")
-//    void createCard(){
-//        List<domain.Card.Card> cardList= new ArrayList<>();
-//        Cards cards = new Cards(cardList);
-//        assertEquals(cardList.size(),52);
-//        for(int i=0;i<cards.getCards().size();i++) {
-//            System.out.println(cards.getCards().get(i).getCardShape().getShape());
-//            System.out.println(cards.getCards().get(i).getPattern().getName());
-//        }
-//    }
+    @Test
+    @DisplayName("Make Cards")
+    void createCard(){
+        List<Card> cardList= new ArrayList<>();
+        Cards cards = new Cards(cardList);
+        assertEquals(cardList.size(),52);
+        for(int i=0;i<cards.getCards().size();i++) {
+            System.out.println(cards.getCards().get(i).getCardShape().getShape());
+            System.out.println(cards.getCards().get(i).getPattern().getName());
+        }
+    }
 
 
 }
